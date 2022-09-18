b39a3e44133fc54d2e31df69b4560adb4fd34fba
karl-maker <karljohanbailey98@gmail.com>
2022-09-18 19:59:54 -0400

    A

--- models/virtual-space.js
+++ models/virtual-space.js
@@ -7,7 +7,7 @@ const VirtualSpaceSchema = new mongoose.Schema(
   name: { type: String, default: "" },
   description: { type: String, default: "" },
   host: { type: String, required: true },
-  time_limit: { type: Number, default: 360 },
+  time_limit: { type: Number, default: 90 },
   current_amount_attending: { type: Number, default: 0 },
   private: { type: Boolean, default: 0 },
   password: { type: String },
