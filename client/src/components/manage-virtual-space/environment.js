b8731bcae9c8fb439a904a6354f26ea3336b1dff
karl-maker <karljohanbailey98@gmail.com>
2022-09-20 09:23:58 -0400

    File Limit

--- client/src/components/manage-virtual-space/environment.js
+++ client/src/components/manage-virtual-space/environment.js
@@ -93,7 +93,7 @@ export default function EnvironmentSelection({ virtualSpace, open, setOpen }) {
               />
               <input
                 type="file"
-                accept="model/glb, model/gltf, model/babylon, model/obj, model/stl"
+                accept=".glb, .gltf, .babylon, .obj, .stl"
                 ref={fileInput}
                 style={{ display: "none" }}
                 onChange={() => {
