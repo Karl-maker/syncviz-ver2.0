21342d0887e54656721185519d536f54e012de08
karl-maker <karljohanbailey98@gmail.com>
2022-10-06 22:44:35 -0400

    K

--- client/src/utils/constants/url.js
+++ client/src/utils/constants/url.js
@@ -1,6 +1,7 @@
 const URLS = {
   feedback_survey: "https://qy1dv96qc5r.typeform.com/to/PHOgyVlt",
-  presentation: "https://my.visme.co/view/rxmqwd38-ke7lp9yn40x429mw",
+  presentation:
+    "https://www.canva.com/design/DAFOSyk_Ogk/KZvXUSP61x_gED8yeEG2Eg/view",
 };
 
 export default URLS;
